0974344d721a141c7cab90e5ff3e32ac40d33a92
James Slopey <181002700+j-slopey@users.noreply.github.com>
2024-12-29 09:55:28 -0600

    completed book exercise

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function Book(title, author, pageCount) {
     this.title = title;
     this.author = author;
     this.pageCount = pageCount;
-    function info(){
-        return "Title: " + " Author: " + " Pages: " + pageCount;
+    this.info =  function () {
+        return "Title: " + title + " - Author: " + author + " - Pages: " + pageCount;
     }
 }
 theHobbit = new Book("The Hobbit", "John Ronald Reuel Tolkien", 310);
-console.log(theHobbit.author)
+console.log(theHobbit.info())
